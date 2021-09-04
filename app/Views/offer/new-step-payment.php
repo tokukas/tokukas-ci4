180f4689a14419ace87b1a118a62a64128bc4a02
Fityandhiya Islam Nugroho <fityandhiya@gmail.com>
2021-09-04 15:21:44 +0700

    remove unused code in payment page

--- app/Views/offer/new-step-payment.php
+++ app/Views/offer/new-step-payment.php
@@ -16,7 +16,7 @@
         <form action="<?= base_url('offer/new/4'); ?>" method="post">
             <?= csrf_field(); ?>
 
-            <!-- STEP 3 -->
+            <!-- STEP 3.1 -->
             <div class="row mb-3">
                 <h2>Pilih Metode Pembayaran</h2>
                 <p>Metode pembayaran mana yang harus kami gunakan untuk mengirimkan uang pembayaran buku anda?</p>
@@ -104,26 +104,6 @@
                 </div>
             </div>
 
-            <!-- <div id="transferDestField" class="row mb-3 d-none">
-                <div class="col-md-12 col-lg-6 mb-3">
-                    <div class="form-input">
-                        <label for="destNumInput" class="form-label"></label>
-                        <div class="input-group">
-                            <div class="input-group-text">
-                                <i id="icon" class="fas fa-money-check"></i>
-                            </div>
-                            <input type="tel" name="payment_destination" id="destNumInput" class="form-control">
-                        </div>
-                        <div class="form-text">Gunakan nomor telepon yang terdaftar pada layanan pembayaran yang anda pilih.</div>
-                    </div>
-                </div>
-                <div class="col-md-12 col-lg-6 mb-3">
-                    <p class="fw-bold text-danger mb-2">Pastikan nomor telepon atau nomor rekening yang anda masukkan benar!</p>
-                    <p style="font-size: smaller;">TOKUKAS <strong>tidak akan bertanggung jawab</strong> atas semua kerugian akibat kesalahan nomor telepon atau nomor rekening yang anda berikan.</p>
-                </div>
-            </div> -->
-
-
             <div class="row">
                 <div class="btn-navigate">
                     <a href="<?= base_url('offer/new/3'); ?>" class="btn btn-outline-secondary">
@@ -145,7 +125,5 @@
 
 <!-- CUSTOM SCRIPTS SECTION -->
 <?= $this->section('custom-scripts'); ?>
-
-<script src="<?= base_url('scripts/js/offer-new-payment.js'); ?>" defer></script>
 
 <?= $this->endSection(); ?>
